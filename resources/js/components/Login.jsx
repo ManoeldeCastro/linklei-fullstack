db18556bf1887d6dc6bab62590aa6cbd8c10a6c8
manoel <manoeldiasmf@gmail.com>
2023-11-30 17:19:37 -0300

    feat: layout style

--- resources/js/components/Login.jsx
+++ resources/js/components/Login.jsx
@@ -19,10 +19,10 @@ function Login({ onLoginSuccess }) {
 };
 
   return (
-    <div className="container mt-5">
+    <div className="container my-5 ">
       <div className="row justify-content-center">
-        <div className="col-md-6">
-          <div className="card">
+        <div className="col-md-6 ">
+          <div className="card shadow">
             <div className="card-header">
               <h2 className="text-center">Login</h2>
             </div>
--- resources/js/components/Signup.jsx
+++ resources/js/components/Signup.jsx
@@ -15,10 +15,10 @@ function Signup({ onSignupSuccess }) {
   };
 
   return (
-    <div className="container mt-5">
+    <div className="container my-5">
       <div className="row justify-content-center">
         <div className="col-md-6">
-          <div className="card">
+          <div className="card  shadow">
             <div className="card-header">
               <h2 className="text-center">Cadastro</h2>
             </div>
